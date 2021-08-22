919cc81fb60c7f49adf07ff1764cdafc24d09ade
Arthur <arthur.hamel1@orange.fr>
2021-08-22 13:58:13 +0200

    fix cors

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 const { graphqlHTTP } = require("express-graphql");
 const port = 4201;
 const server = express();
 const schema = require("./graphql/schema");
 
+const corsOptions = {
+  origin: "*",
+};
+
+server.use(cors(corsOptions));
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
 
